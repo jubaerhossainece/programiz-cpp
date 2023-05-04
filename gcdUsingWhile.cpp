1c482f1625eda456957424ef9c451fc199a1e68f
jubaerhossainece <jubaerkuet@gmail.com>
2023-05-04 23:20:57 +0600

    gcf using for and while loop

--- gcdUsingWhile.cpp
+++ gcdUsingWhile.cpp
@@ -11,12 +11,36 @@ int main(){
     int n2;
     cin >> n2;
 
-    int gcdUsingWhile(n1,n2);
+    int gcd;
+  
+    gcd = gcdUsingWhile(n1, n2);
+
+    cout << "GCD is : " << gcd << endl;
 }
 
 int gcdUsingWhile(int n1, int n2){
 
-    int gcd;
+    int mod;
+    int gcd = 1;
+    if (n1 > n2)
+    {
+        int temp = n1;
+        n1 = n2;
+        n2 = temp;
+    }
 
-    
+    while(n1 > 1){
+
+        mod = n2 % n1;
+
+        if(mod == 0){
+            gcd = n1;
+            break;
+        }
+
+        n1 = mod;
+        n2 = n1;
+    }
+
+    return gcd;
 }
