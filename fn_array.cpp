6315b09d828231ed21f210fc7cc9819b1274ee39
jubaer-riseuplabs <jubaer.riseuplabs@gmail.com>
2023-05-08 18:01:38 +0600

    string done

--- fn_array.cpp
+++ fn_array.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 
+// declare a function to display marks
+// take a 1d array as parameter
+int display(int marks[5]){
+    cout << "Displaying marks:" << endl;
+
+    //display array elements
+    int size = sizeof(marks);
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Marks: " << marks[i] << endl;
+    }
+    return 0;
+}
+
 int main(){
-    
+    int a[5] = {23,45,67,78,66};
+
+    display(a);
 }
---
+++ string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string str;
+    cout << "Enter a string:";
+    getline(cin, str);
+
+    cout << "string is:" << endl << str;
+    return 0;
+}
